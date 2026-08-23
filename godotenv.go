a34ef54e52bf8c7134b926ebac2a164324c169af
382868331 <382868331@users.noreply.github.com>
2026-08-24 06:11:58 +0800

    test: reproduce 输出映射初始化

--- godotenv.go
+++ godotenv.go
@@ -111,7 +111,7 @@ func Unmarshal(str string) (envMap map[string]string, err error) {
 
 // UnmarshalBytes parses env file from byte slice of chars, returning a map of keys and values.
 func UnmarshalBytes(src []byte) (map[string]string, error) {
-	out := make(map[string]string)
+	var out map[string]string
 	err := parseBytes(src, out)
 
 	return out, err
