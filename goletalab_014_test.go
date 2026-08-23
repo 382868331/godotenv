7e1696a4f3d10168e9867a2d4351db728b63eb7f
382868331 <382868331@users.noreply.github.com>
2026-08-24 06:20:33 +0800

    test: reproduce YAML冒号声明

---
+++ goletalab_014_test.go
@@ -0,0 +1,4 @@
+package godotenv
+import("os";"path/filepath";"strings";"testing")
+var _=os.Getenv;var _=filepath.Join;var _=strings.Contains
+func TestGoletaGodotenv014(t *testing.T){m,e:=Unmarshal("A: value");if e!=nil||m["A"]!="value"{t.Fatalf("m=%v err=%v",m,e)}}
